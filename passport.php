76fcdb70307d64a749dd772fead0b05567941756
ThanhThien07 <slen010207@gmail.com>
2026-06-30 01:18:07 +0700

    t

--- passport.php
+++ passport.php
@@ -134,7 +134,7 @@ if (!$passport) {
                             <img class="w-full h-auto block select-none pointer-events-none" src="<?php echo $thumoi_base64 ?: 'anh/default.png'; ?>" alt="Vé Thư Mời">
                             
                             <!-- Họ tên đè lên (Overlay) - Chân chữ sát dòng dấu chấm bằng Tailwind CSS -->
-                            <div class="absolute top-[47.5%] left-[26%] w-[61%] text-center leading-none whitespace-nowrap overflow-hidden text-[#5c1d0c] font-serif italic font-bold pointer-events-none z-10" id="overlay-tm-name">
+                            <div class="absolute top-[46.5%] left-[26%] w-[61%] text-center leading-none whitespace-nowrap overflow-hidden text-[#5c1d0c] font-serif italic font-bold pointer-events-none z-10" id="overlay-tm-name">
                                 <?php echo htmlspecialchars($fullname); ?>
                             </div>
                         </div>
